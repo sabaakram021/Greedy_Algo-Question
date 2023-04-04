c01b8e65bd42fd5f882a59f5076a98a7ca821707
saba akram <sabaakram211@gmail.com>
2023-04-04 11:50:11 +0530

    sorting based on end array using 2d array and lambda function

--- Activity_Selection.java
+++ Activity_Selection.java
@@ -1,23 +1,43 @@
 import java.util.*;
 class Activity_Selection{
     public static void main(String[] args) {
-        int[] start = {1, 3, 0, 5, 8, 5};
-        int[] end = {2, 4, 6, 7, 9, 9};   // end time arr sorted in ascending order if it 
+        //end time sorted
+        // int[] start = {1, 3, 0, 5, 8, 5};
+        // int[] end = {2, 4, 6, 7, 9, 9};   // end time arr sorted in ascending order if it 
         // not sorted, first we need to sort this end arr in ascending order.
 
+        int[] start = {1, 3, 2, 5};
+        int[] end = {2, 4, 3, 6};
+        //Sorting arr based on end time in increasing order
+        int[][ ] acitivities = new int[start.length][3];
+        for(int i=0; i<start.length; i++){
+            acitivities[i][0] = i;
+            acitivities[i][1] = start[i];
+            acitivities[i][2] = end[i];
+        }
+        Arrays.sort(acitivities,Comparator.comparingDouble(o->o[2]));
         int maxAct=1;
         ArrayList<Integer> ans = new ArrayList<>();
 
         // 1st Activity
         maxAct=1;
-        ans.add(0); // first activity index
-        int lastEnd = end[0];
+        // ans.add(0); // first activity index
+        ans.add(acitivities[0][0]);
+        // int lastEnd = end[0];
+        int lastEnd = acitivities[0][2];
 
-        for(int i=1; i<end.length; i++){
-            if(start[i]>=lastEnd){
+        // for(int i=1; i<end.length; i++){
+        //     if(start[i]>=lastEnd){
+        //         maxAct+=1;
+        //         ans.add(i);
+        //         lastEnd=end[i];
+        //     }
+        // }
+        for(int i=1; i<acitivities.length; i++){
+            if(acitivities[i][1] > lastEnd){
                 maxAct+=1;
-                ans.add(i);
-                lastEnd=end[i];
+                ans.add(acitivities[i][0]);
+                lastEnd = acitivities[i][2]; 
             }
         }
 
